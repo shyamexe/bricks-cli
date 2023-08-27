f7c0f9e9e3deac8563a2a896f6639b670000dc15
shyamexe <shyamjith38@hotmail.com>
2023-08-27 14:49:24 +0530

    added feature bloc

---
+++ feature-block/__brick__/{{name.snakeCase()}}/models/{{name.snakeCase()}}_models.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class {{name.pascalCase()}}Model {
+  final String? name ;
+  {{name.pascalCase()}}Model({
+    required this.name,
+  });
+  
+}
---
+++ feature-block/__brick__/{{name.snakeCase()}}/screens/{{name.snakeCase()}}_screens.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class name extends StatelessWidget {
+  const name({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text({{name}}),
+      ));
+  }
+}
