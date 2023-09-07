0d3c2a6ba1e649b82d6bee84bd41e3f67fd7da62
Shyamjith <93277108+shyamexe@users.noreply.github.com>
2023-09-07 17:00:55 +0530

    Update widget_test.dart

--- flutter-template/__brick__/{{app-name.paramCase()}}/test/widget_test.dart
+++ flutter-template/__brick__/{{app-name.paramCase()}}/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:{{app-name.paramCase()}}/main.dart';
+import 'package:{{app-name.pascalCase()}}/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
